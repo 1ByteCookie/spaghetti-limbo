3a031c0d6c1613a20a342ac1c41737d404285d2f
1ByteCookie <68849067+1ByteCookie@users.noreply.github.com>
2022-05-01 21:43:00 +0800

    test triangle

--- spaghetti-limbo/Src/Application.cpp
+++ spaghetti-limbo/Src/Application.cpp
@@ -5,11 +5,45 @@ Application Application::Instance;
 
 int Application::OnStart()
 {
+	float VertexData[] =
+	{
+		 0.0f,   1.0f,
+		-1.0f,  -1.0f,
+		 1.0f,  -1.0f
+	};
+
+	uint32_t IndexData[] =
+	{
+		0, 1, 2
+	};
+
+
+	GLuint VAO, VBO, IBO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData), VertexData, GL_STATIC_DRAW);
+
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IndexData), IndexData, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 	glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
 	while (!glfwWindowShouldClose(m_Window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
+
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(m_Window);
 	}
